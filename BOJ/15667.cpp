34eace90bdbb755daecbe13b30708d8d7afe7a72
Sunghoon <ghdtjdgns02@naver.com>
2018-08-03 22:58:30 +0900

    solved

---
+++ BOJ/15667.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <cstdio>
+#include <string>
+#include <string.h>
+#include <array>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <utility>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vint;
+typedef vector<vint> vvint;
+typedef vector<bool> vbool;
+typedef vector<vbool> vvbool;
+typedef deque<int> dqint;
+
+inline bool isNum(char ch) {return ch>='0'&&ch<='9';}
+inline int getInt() {
+    int ret = 0, ch;
+    bool sign = 0;
+    for(ch=getchar(); !(isNum(ch)||ch=='-'); ch=getchar());
+    if(ch=='-') { sign = 1; ch=getchar(); }
+    if(!isNum(ch)) return getInt(); // InvalidInputError
+    else for(ret=ch-'0', ch=getchar(); isNum(ch); ch=getchar()) ret = ret*10 + ch-'0';
+    return sign?-ret:ret;
+}
+inline ll getLL() {ll n; scanf("%lld", &n); return n;}
+inline ull getULL() {ull n; scanf("%llu", &n); return n;}
+inline void putInt(int n) {printf("%d", n);}
+inline void enter() {putchar('\n');}
+
+/*
+    k^2+k+1 = n
+*/
+
+int main() {
+    int n, k;
+    n = getInt();
+    for(k=1; k*k+k+1<n; ++k);
+    if(k*k+k+1==n) {
+        putInt(k);
+    }
+    return 0;
+}
