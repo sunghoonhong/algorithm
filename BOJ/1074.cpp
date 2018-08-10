2b91d398c876dc9e4a0475cc41a29558cf8cc6cf
Sunghoon <ghdtjdgns02@naver.com>
2018-08-10 22:55:41 +0900

    solved (improved)

--- BOJ/1074.cpp
+++ BOJ/1074.cpp
@@ -53,8 +53,9 @@ inline void enter() {putchar('\n');}
     N=1일때는
     01
     23
-    F(x,y,1): (y,x)부터 N=1일때 순서를 쓰면,
-    (y,x), (y,x+1), (y+1,x), (y+1,x+1)
+    F(x,y,0): (x,y)==(c,r) 인가?
+    F(x,y,1): (x,y)부터 N=1일때 순서를 쓰면,
+    (x,y), (x+1,y), (x,y+1), (x+1,y+1)
     순으로 방문한다.
     F(x,y,2)=
     F(x,y,1),F(x+2,y,1),F(x,y+2,1),F(x+2,y+2,1)
@@ -66,33 +67,47 @@ inline void enter() {putchar('\n');}
     순진하게 처음부터 끝까지 순회하지않고,
     분할시에 어떤 사분면에 속하는 지에 따라
     시작위치를 한번에 옮길수 있다.
-
 */
 
 int ans = 0;
 bool solved = false;
-int r, c;
+int c, r;
 
 void drawZ(int x, int y, int n) {
+    // printf("n=%d: %d, %d: %d\n", n,x,y,ans);
     if(solved) return;
-    if(n==1) {
-        for(int i=0; i<2; ++i) {
-            for(int j=0; j<2; ++j) {
-                // printf("%d, %d: %d\n",x+j, y+i,ans);
-                if(x+j==c&&y+i==r) {
-                    solved = true;
-                    return;
-                }
-                ans++;
-            }
-        }
+    if(n==0) {
+        if(x==c&&y==r) solved = true;
     }
     else {
         int d=1;
         for(int i=0; i<n-1; ++i, d*=2);
-        for(int i=0; i<2; ++i) {
-            for(int j=0; j<2; ++j) {
-                drawZ(x+d*j,y+d*i,n-1);
+        /*
+        (x,y,n-1)           (x+d,y,n-1):
+        c>=x && c<x+d       c>=x+d && c<x+2*d
+        r>=y && r<y+d       r>=y && r<y+d
+                        
+        (x,y+d,n-1):        (x+d,y+d,n-1):
+        c>=x && c<x+d       c>=x+d && c<x+2*d
+        r>=y+d && r<y+2*d   r>=y+d && r<y+2*d
+        */
+        if(c>=x+d) {
+            if(r>=y+d) {
+                ans+=3*d*d;
+                drawZ(x+d,y+d,n-1);
+            }
+            else {
+                ans+=d*d;
+                drawZ(x+d,y,n-1);
+            }
+        }
+        else {
+            if(r>=y+d) {
+                ans+=2*d*d;
+                drawZ(x,y+d,n-1);
+            }
+            else {
+                drawZ(x,y,n-1);
             }
         }
     }
